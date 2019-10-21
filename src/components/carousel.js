beb87abf33800f0928fe8953697a83d41080b630
u7579 <u7579@212fe3e1-850d-44eb-bb0b-4eb50585c1a2>
2019-10-21 06:02:45 +0000

    Automatic commit to update pictures

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,21 +1,19 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 
-import Truck from "./images/carousel/Truck";import Closeup from "./images/carousel/Closeup";import OrganicSign from "./images/carousel/OrganicSign";import Tree2 from "./images/carousel/Tree2";import Trees1 from "./images/carousel/Trees1";import PhotoOct0134433PM from "./images/carousel/PhotoOct0134433PM";import Trees3 from "./images/carousel/Trees3";import PhotoOct1024230PM from "./images/carousel/PhotoOct1024230PM";const MyCarousel = () => (
+import Trees1 from "./images/carousel/Trees1";import Trees3 from "./images/carousel/Trees3";import OrganicSign from "./images/carousel/OrganicSign";import Truck from "./images/carousel/Truck";import Tree2 from "./images/carousel/Tree2";import Closeup from "./images/carousel/Closeup";import PhotoOct1024230PM from "./images/carousel/PhotoOct1024230PM";const MyCarousel = () => (
     <Carousel>        <Carousel.Item>
-            <Truck/>
+            <Trees1/>
         </Carousel.Item>        <Carousel.Item>
-            <Closeup/>
+            <Trees3/>
         </Carousel.Item>        <Carousel.Item>
             <OrganicSign/>
         </Carousel.Item>        <Carousel.Item>
+            <Truck/>
+        </Carousel.Item>        <Carousel.Item>
             <Tree2/>
         </Carousel.Item>        <Carousel.Item>
-            <Trees1/>
-        </Carousel.Item>        <Carousel.Item>
-            <PhotoOct0134433PM/>
-        </Carousel.Item>        <Carousel.Item>
-            <Trees3/>
+            <Closeup/>
         </Carousel.Item>        <Carousel.Item>
             <PhotoOct1024230PM/>
         </Carousel.Item></Carousel>
