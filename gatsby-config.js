0efca59f4cf5e566d27ff43e91b0907be3806cc1
Raymond DeVries <raymond.l.devries@gmail.com>
2019-11-18 16:41:28 -0800

    Changing some config settings.

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,11 @@ module.exports = {
         {
             resolve: `gatsby-plugin-manifest`,
             options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
+                name: `Nookachamps Farms`,
+                short_name: `Nookachamps`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#111111`,
+                theme_color: `#203671`,
                 display: `minimal-ui`,
                 icon: `src/images/globe.png` // This path is relative to the root of the site.
             }
