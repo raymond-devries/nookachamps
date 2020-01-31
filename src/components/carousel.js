37243aeb6faa18965cd930294e367db82273fa17
u3797 <u3797@c3c71ba4-5f2d-489f-b973-378b2ec74750>
2020-01-31 01:49:57 +0000

    Automatic commit to update pictures

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,35 +1,37 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
 
-import PhotoSep2135655PM from "./images/carousel/PhotoSep2135655PM";import Truck from "./images/carousel/Truck";import PhotoJan1310849PM from "./images/carousel/PhotoJan1310849PM";import PhotoSep2553918PM from "./images/carousel/PhotoSep2553918PM";import PhotoOct1111302PM from "./images/carousel/PhotoOct1111302PM";import PhotoSep2554615PM from "./images/carousel/PhotoSep2554615PM";import PhotoDec0410627PM from "./images/carousel/PhotoDec0410627PM";import PhotoOct3132705PM from "./images/carousel/PhotoOct3132705PM";import Trees3 from "./images/carousel/Trees3";import OrganicSign from "./images/carousel/OrganicSign";import Tree2 from "./images/carousel/Tree2";import Trees1 from "./images/carousel/Trees1";import PhotoOct1024230PM from "./images/carousel/PhotoOct1024230PM";import Closeup from "./images/carousel/Closeup";const MyCarousel = () => (
+import PhotoOct1111302PM from "./images/carousel/PhotoOct1111302PM";import PhotoDec0410627PM from "./images/carousel/PhotoDec0410627PM";import Truck from "./images/carousel/Truck";import PhotoOct1024230PM from "./images/carousel/PhotoOct1024230PM";import PhotoSep2553918PM from "./images/carousel/PhotoSep2553918PM";import OrganicSign from "./images/carousel/OrganicSign";import PhotoOct3132705PM from "./images/carousel/PhotoOct3132705PM";import Trees1 from "./images/carousel/Trees1";import PhotoSep2554615PM from "./images/carousel/PhotoSep2554615PM";import Tree2 from "./images/carousel/Tree2";import Closeup from "./images/carousel/Closeup";import PhotoSep2135655PM from "./images/carousel/PhotoSep2135655PM";import Trees3 from "./images/carousel/Trees3";import PhotoOct0911600PM from "./images/carousel/PhotoOct0911600PM";import PhotoJan1310849PM from "./images/carousel/PhotoJan1310849PM";const MyCarousel = () => (
     <Carousel>        <Carousel.Item>
-            <PhotoSep2135655PM/>
-        </Carousel.Item>        <Carousel.Item>
-            <Truck/>
-        </Carousel.Item>        <Carousel.Item>
-            <PhotoJan1310849PM/>
-        </Carousel.Item>        <Carousel.Item>
-            <PhotoSep2553918PM/>
-        </Carousel.Item>        <Carousel.Item>
             <PhotoOct1111302PM/>
-        </Carousel.Item>        <Carousel.Item>
-            <PhotoSep2554615PM/>
         </Carousel.Item>        <Carousel.Item>
             <PhotoDec0410627PM/>
         </Carousel.Item>        <Carousel.Item>
-            <PhotoOct3132705PM/>
-        </Carousel.Item>        <Carousel.Item>
-            <Trees3/>
-        </Carousel.Item>        <Carousel.Item>
-            <OrganicSign/>
-        </Carousel.Item>        <Carousel.Item>
-            <Tree2/>
-        </Carousel.Item>        <Carousel.Item>
-            <Trees1/>
+            <Truck/>
         </Carousel.Item>        <Carousel.Item>
             <PhotoOct1024230PM/>
         </Carousel.Item>        <Carousel.Item>
+            <PhotoSep2553918PM/>
+        </Carousel.Item>        <Carousel.Item>
+            <OrganicSign/>
+        </Carousel.Item>        <Carousel.Item>
+            <PhotoOct3132705PM/>
+        </Carousel.Item>        <Carousel.Item>
+            <Trees1/>
+        </Carousel.Item>        <Carousel.Item>
+            <PhotoSep2554615PM/>
+        </Carousel.Item>        <Carousel.Item>
+            <Tree2/>
+        </Carousel.Item>        <Carousel.Item>
             <Closeup/>
+        </Carousel.Item>        <Carousel.Item>
+            <PhotoSep2135655PM/>
+        </Carousel.Item>        <Carousel.Item>
+            <Trees3/>
+        </Carousel.Item>        <Carousel.Item>
+            <PhotoOct0911600PM/>
+        </Carousel.Item>        <Carousel.Item>
+            <PhotoJan1310849PM/>
         </Carousel.Item></Carousel>
 );
 
