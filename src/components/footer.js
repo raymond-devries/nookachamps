fadb93b28a7264580056df7b19026b8a135a53be
Raymond DeVries <51940365+raymond-devries@users.noreply.github.com>
2020-06-07 10:54:28 -0700

    Update footer year.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
     <div className="p-3 text-center footer-background text-white">
-        <small>&copy; Nookachamps Farms 2019</small>
+        <small>&copy; Nookachamps Farms 2020</small>
     </div>
 );
 
