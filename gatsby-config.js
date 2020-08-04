60ae7f58f363a4c7275fcea1d0f6c72b5febf7d4
raymond-devries <raymond.l.devries@gmail.com>
2020-08-04 15:54:03 -0700

    Update favicon to logo.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
                 background_color: `#111111`,
                 theme_color: `#203671`,
                 display: `minimal-ui`,
-                icon: `src/images/globe.png` // This path is relative to the root of the site.
+                icon: `src/images/logo.png` // This path is relative to the root of the site.
             }
         }
         // this (optional) plugin enables Progressive Web App + Offline functionality
